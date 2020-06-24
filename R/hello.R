0b6887c0a9636ad4fb3e8071078b086b835d4741
Donald R. Williams <brycewayne03.29@gmail.com>
2020-06-24 15:44:34 -0700

    add src

--- R/hello.R
+++ R/hello.R
@@ -12,8 +12,8 @@
 #   Install Package:           'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
-
 hello <- function() {
 
-  print("Hello, world!")
+  print("Hello,d world!")
+
 }
